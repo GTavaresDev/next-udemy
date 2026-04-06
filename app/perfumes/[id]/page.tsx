0805f12ce4400e38238ca286dacd18d2531a3ed3
Tavares <tavares.gabriel.dev@gmail.com>
2026-04-06 11:30:46 -0300

    Add perfumes listing and detail pages with data structure
    - Created PerfumesPage for displaying a list of perfumes.
    - Implemented PerfumePage for individual perfume details.
    - Added data structure for perfumes in data/perfums.ts.

---
+++ app/perfumes/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link';
+import { perfums } from '../data/perfums';
+
+const PerfumePage = () => {
+  return (
+    <div>Perfume J</div>
+  )
+}
+
+export default PerfumePage;
